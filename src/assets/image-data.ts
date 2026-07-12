b4c5c9c5e10fd4b9db9dbda3dea996c750705d2d
zydalrymyahtyat-debug <zydalrymyahtyat@gmail.com>
2026-07-12 15:18:41 +0300

    refactor: migrate assets to local paths and update build script

--- src/assets/image-data.ts
+++ src/assets/image-data.ts
@@ -1,7 +1,7 @@
-export const quranImg = "https://upload.wikimedia.org/wikipedia/commons/f/f4/Quran.jpg";
-export const hisnImg = "https://upload.wikimedia.org/wikipedia/commons/a/a1/Dua.jpg";
-export const misbahaImg = "https://upload.wikimedia.org/wikipedia/commons/c/cd/Masbaha.jpg";
-export const prayerImg = "https://upload.wikimedia.org/wikipedia/commons/e/e5/Mosque.jpg";
-export const libraryImg = "https://upload.wikimedia.org/wikipedia/commons/1/18/Two_bookshelves_full_of_books_belonging_to_Unitedmissionary_%282010%29.jpg";
-export const storiesImg = "https://upload.wikimedia.org/wikipedia/commons/8/87/Desert.jpg";
-export const bannerImg = "https://upload.wikimedia.org/wikipedia/commons/0/06/Kaaba.jpg";
+export const quranImg = "/img/quran.jpg";
+export const hisnImg = "/img/hisn.jpg";
+export const misbahaImg = "/img/misbaha.jpg";
+export const prayerImg = "/img/prayer.jpg";
+export const libraryImg = "/img/library.jpg";
+export const storiesImg = "/img/stories.jpg";
+export const bannerImg = "/img/banner.jpg";
