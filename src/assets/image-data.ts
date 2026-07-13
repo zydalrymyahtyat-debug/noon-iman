955604d6ef01b54613e436d5a4baf90a04554ae7
zydalrymyahtyat-debug <zydalrymyahtyat@gmail.com>
2026-07-13 16:44:58 +0300

    refactor: remove legacy image processing scripts
    Delete image conversion script and associated data file as they are no longer required for asset management.

--- src/assets/image-data.ts
+++
@@ -1,9 +0,0 @@
-const quranImg = './img/quran.jpg';
-const hisnImg = './img/hisn.jpg';
-const misbahaImg = './img/misbaha.jpg';
-const prayerImg = './img/prayer.jpg';
-const libraryImg = './img/library.jpg';
-const storiesImg = './img/stories.jpg';
-const bannerImg = './img/banner.jpg';
-
-export { quranImg, hisnImg, misbahaImg, prayerImg, libraryImg, storiesImg, bannerImg };
